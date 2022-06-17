da130118246932db76c8e7d9bd86d5acb9b2bd8a
RaoulBumbasiJr <100057772+RaoulBumbasiJr@users.noreply.github.com>
2022-06-17 11:41:17 -0700

    Add files via upload

---
+++ to-do-list/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>to do list</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ to-do-list/src/components/TodosList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Todoslist = ({ todos, setTodos }) => {
+  return (
+    <div>
+        {todos.map((todo) => (
+            <li className="list-item" key={todo.id}>
+                <input 
+                    type="text" 
+                    value={todo.title} 
+                    className="list"
+                    onChange={(event) => event.preventDefault()}
+                />
+                <div>
+                    <button className="button-complete task-button">
+                        <i className="fa fa-check-circle"></i>
+                    </button>
+                </div>
+            </li>
+        ))}
+    </div>
+  );
+}
+
+export default Todoslist;
